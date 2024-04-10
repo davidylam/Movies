2568f6b00a5304f2dbc233436110c03a91c12696
David Lam <David.Lam@DecisionResearch.com>
2024-04-10 11:35:41 -1000

    Add project files.

---
+++ Movies.Client/Services/CRUDSamples.cs
@@ -0,0 +1,34 @@
+﻿using Movies.Client.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace Movies.Client.Services;
+
+public class CRUDSamples : IIntegrationService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public CRUDSamples(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+    }
+
+    public async Task RunAsync()
+    {
+        await GetResourceAsync();
+    }
+
+    public async Task GetResourceAsync() {
+        var httpClient = _httpClientFactory.CreateClient();
+        httpClient.BaseAddress = new Uri("http://localhost:5001");
+        httpClient.Timeout = new TimeSpan(0, 0, 30);
+
+        var response = await httpClient.GetAsync("api/movies");
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(content,
+            new JsonSerializerOptions { 
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+    }
+}
